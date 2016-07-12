6a68c7bd078d08725fc618f134cff56f6c1ed036
Weierstrass <andreas@bulling.se>
2016-07-12 23:22:07 +0200

    Document ConversionInterface

--- src/conversion/ConversionInterface.hpp
+++ src/conversion/ConversionInterface.hpp
@@ -22,10 +22,33 @@ namespace protobuf_decoder
          */
         virtual std::string encode(const std::string& iDecodedString) = 0;
 
+        /**
+         * @brief Algorithm to apply on encoded string to decode it.
+         *
+         * @param iEncodedString The string to decode.
+         *
+         * @return std::string The decoded string.
+         */
         virtual std::string decode(const std::string& iEncodedString) = 0;
 
+        /**
+         * @brief Sets the encode algorithm to use when encoding.
+         *
+         * The algorithm is implemented using the bla bla interface.
+         *
+         * @param iAlgoName The key for the algorithm.
+         *
+         * @return void
+         */
         virtual void setEncodeAlgorithm(const std::string& iAlgoName) = 0;
 
+        /**
+         * @brief Sets the path to the proto grammar message files.
+         *
+         * @param iMessagePath The path to message files.
+         *
+         * @return void
+         */
         virtual void setMessagePath(const std::string& iMessagePath) = 0;
 
     };
