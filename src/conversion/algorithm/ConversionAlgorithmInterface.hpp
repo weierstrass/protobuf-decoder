03c5446997a5a27e1ecd58f08361afe806596115
Andreas Bülling <andreas@bulling.se>
2016-09-07 13:48:33 +0200

    Update ConversionAlgorithmInterface.hpp

--- src/conversion/algorithm/ConversionAlgorithmInterface.hpp
+++ src/conversion/algorithm/ConversionAlgorithmInterface.hpp
@@ -8,16 +8,46 @@ namespace protobuf_decoder
 {
     namespace algorithm
     {
-        
+        /**
+         * @brief Interface for a conversion algorithm.
+         * 
+         * Visible functions to caller classes. The main
+         * purpose of this interface is to hide the virtual 
+         * constructor implementation for the client.
+         */
         class ConversionAlgorithmInterface
         {
         public:
+            /**
+             * @brief Destructor. 
+             */
             virtual ~ConversionAlgorithmInterface() {}
             
+            /**
+             * @brief Encode implementation for algorithm.
+             * 
+             * @param iString Binary represenation.
+             * 
+             * @return string Encoded representation.
+             */
             virtual std::string encode(const std::string& iString) = 0;
 
+            /**
+             * @brief Decode implementation for algorithm.
+             * 
+             * @param iString Encoded representation.
+             * 
+             * @return string Binary representation.
+             */
             virtual std::string decode(const std::string& iString) = 0;
 
+            /**
+             * @brief Name of algorithm.
+             * 
+             * This is the name that will be seen by the end user.
+             * 
+             * @return string Name.
+             */
             virtual std::string getName() = 0;
         };
         
