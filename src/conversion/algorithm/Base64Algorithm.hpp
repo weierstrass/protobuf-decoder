d799c09d963bdbe152577719ff8b382d7dfcab6f
Andreas Bülling <andreas@bulling.se>
2016-09-07 12:41:34 +0200

    Update Base64Algorithm.hpp

--- src/conversion/algorithm/Base64Algorithm.hpp
+++ src/conversion/algorithm/Base64Algorithm.hpp
@@ -7,7 +7,14 @@ namespace protobuf_decoder
 {
     namespace algorithm
     {
-        
+        /**
+         * @brief Algorithm handling base64 encoding/decoding.
+         * 
+         * Standard implementation using + and / as extra characters 
+         * and = for padding.
+         * 
+         * https://en.wikipedia.org/wiki/Base64
+         */
         class Base64Algorithm : public BaseAlgorithm<Base64Algorithm>
         {
         public:
