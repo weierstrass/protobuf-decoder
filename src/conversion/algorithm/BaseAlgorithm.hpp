381b068619840c5ffb6e83f29f02d02ee014e121
Andreas Bülling <andreas@bulling.se>
2016-09-07 12:46:41 +0200

    Update BaseAlgorithm.hpp

--- src/conversion/algorithm/BaseAlgorithm.hpp
+++ src/conversion/algorithm/BaseAlgorithm.hpp
@@ -5,6 +5,14 @@
 
 #include <iostream>
 
+/**
+ * @brief Macro used to register an algorithm.
+ * 
+ * A static exemplar of the aglorithm will be added in the
+ * ConversionAlgorithm class with the corresponding key provided.
+ * ConversionAlgorithm is managing the construction of new algorihtm 
+ * objects from the key. (Virtual constructor pattern.)
+ */
 #define REGISTER_ALGORITHM(CLASS, KEY) \
 template <>\
 std::string protobuf_decoder::algorithm::BaseAlgorithm<CLASS>::_key(KEY); \
@@ -15,10 +23,19 @@ namespace protobuf_decoder
 {
     namespace algorithm
     {
+        /**
+         * @brief Base class for new algorithms.
+         * 
+         * Handles common parts related to the registration
+         * of algorithms.
+         * 
+         * @tparam T Algorithm class (CRTP)
+         */
         template <typename T>
         class BaseAlgorithm : public ConversionAlgorithm
         {
         public:
+            
             BaseAlgorithm() : ConversionAlgorithm(_key) {}
 
             virtual ~BaseAlgorithm() {}
